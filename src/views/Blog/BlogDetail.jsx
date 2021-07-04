a0fc00bf791e02727ccf543e5a7a9469c33342ff
mJiyan <m.jiyan_aslan@hotmail.com>
2021-07-04 16:20:42 +0300

    try to husky functionality

--- src/views/Blog/BlogDetail.jsx
+++ src/views/Blog/BlogDetail.jsx
@@ -20,12 +20,12 @@ const BlogDetail = (props) => {
       const { success, error } = await dispatch(actions.getBlogDetails(id));
       if (!success) {
 
-
-            window.console.log('An error occurred with', error.status, error.statusText, 'status');
+        
+        window.console.log('An error occurred with', error.status, error.statusText, 'status');
       }
-    };
+    };      
     loadBlog();
-  }, [dispatch, id]);
+  }, [dispatch, id]       );
 
   return (
     <Container>
