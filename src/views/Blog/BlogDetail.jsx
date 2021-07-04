2f952b75cca35f189d60b19f1d141a38e1b29b35
mJiyan <m.jiyan_aslan@hotmail.com>
2021-07-04 16:25:19 +0300

    trying to husky functionality

--- src/views/Blog/BlogDetail.jsx
+++ src/views/Blog/BlogDetail.jsx
@@ -19,12 +19,11 @@ const BlogDetail = (props) => {
     const loadBlog = async () => {
       const { success, error } = await dispatch(actions.getBlogDetails(id));
       if (!success) {
-            
-            window.console.log('An error occurred with', error.status, error.statusText, 'status');
+                window.console.log('An error occurred with', error.status, error.statusText, 'status');
       }
-    };      
+    };
     loadBlog();
-  }, [dispatch, id]       );
+  }, [dispatch, id]);
 
   return (
     <Container>
